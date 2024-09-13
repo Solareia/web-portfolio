243454142fd3bb1e9a4671861fb599fd1af3b982
Solareia <business.yasinskiyartur@outlook.com>
2024-09-12 22:03:44 -0700

    Small hotfix for github links

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
                     PROJECTS
                 </div>
                 <div className="block md:flex md:justify-center place-items-center h-3/4">
-                    <Link href={"#"} className="md:w-2/12 flex items-center justify-center text-center h-2/5 shadow-md transition-shadow duration-500 hover:shadow-xl rounded-md mx-10 my-2 md:p-4 md:h-3/4 bg-[#e6e7ed] text-[#40434f]">
+                    <Link href={"https://github.com/Solareia/gsod-visualizer"} className="md:w-2/12 flex items-center justify-center text-center h-2/5 shadow-md transition-shadow duration-500 hover:shadow-xl rounded-md mx-10 my-2 md:p-4 md:h-3/4 bg-[#e6e7ed] text-[#40434f]">
                         <div className="block">
                             <p className="my-4">GSOD Visualizer</p>
                             <p className="font-thin px-8">A graphical visualizer for GSOD (Global Surface Summary of the Day), which automatically downloads historical weather data, graphs it, and can be saved.</p>
