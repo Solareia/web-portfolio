47bdaf20b8ffc037a97c51c873ffb993b6886785
Solareia <business.yasinskiyartur@outlook.com>
2024-09-26 15:08:27 -0700

    Updated project components and adjusted alias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Page() {
                         <Image src='/avatar.jpg' width={200} height={200} alt="Avatar" className="rounded-full aspect-square object-cover"/>
                     </div>
                     <p className="flex justify-center items-center w-full text-[#565f89] font-semilight">ARTUR YASINSKIY</p>
-                    <p className="text-6xl flex justify-center my-4 bg-gradient-to-r from-yellow-300 via-blue-400 to-green-400 bg-clip-text text-transparent">SOLAREIA</p>
-                    <p className="flex justify-center items-center w-full font-thin text-[#565f89] mb-4">I focus on embedded systems, networking, and security.</p>
+                    <p className="text-6xl flex justify-center my-4 bg-gradient-to-r from-yellow-300 via-blue-400 to-green-400 bg-clip-text text-transparent">AriaLeFey</p>
+                    <p className="flex justify-center items-center text-center w-full font-thin text-[#565f89] mb-4 md:text-lg text-sm">I focus on embedded systems, networking, and security.</p>
                     <div className="flex justify-center items-center text-2xl space-x-2 text-latte_yellow dark:text-tokyogreen">
                         <Link href={"https://github.com/Solareia"} className="hover:text-[#689a32] transition-all ease-in-out delay-150 ">
                             <AiFillGithub />
